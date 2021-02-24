b986ff4ef32ec2857b0bdc433b72a45f7970aeb0
BronzeIsEnough <37735220+BronzeIsEnough@users.noreply.github.com>
2021-02-24 10:43:33 -0500

    Add in a module for our checks that we would like to enforce for checkstyle.

---
+++ WeaknessGenerator/src/main/java/com/medal/bronze/jsnader/arkhamweakness/checkstyle/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This section is meant to provide checks for Checkstyle on Java files even though the project is
+ * currently designed to be built with Kotlin.  The information in this module will likely not be
+ * used in the project at all.  It is simply meant to cover the majority of cases that Checkstyle is
+ * designed for so that we can import these settings to another project in the future.
+ *
+ * @author jsnader
+ * @version 1.0
+ */
+package com.medal.bronze.jsnader.arkhamweakness.checkstyle;
