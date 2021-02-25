0e6f54fd70e414c5b0475cc8f811d5092c9215c1
BronzeIsEnough <37735220+BronzeIsEnough@users.noreply.github.com>
2021-02-25 11:09:40 -0500

    Update the location file to have a connection type and connection locations associated with it.

--- WeaknessGenerator/src/main/java/com/medal/bronze/jsnader/arkhamweakness/locations/Location.kt
+++ WeaknessGenerator/src/main/java/com/medal/bronze/jsnader/arkhamweakness/locations/Location.kt
@@ -1,20 +1,59 @@
+/**
+ * Copyright 2018-2021 Jeremiah Snader
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.medal.bronze.jsnader.arkhamweakness.locations
 
 import com.medal.bronze.jsnader.arkhamweakness.Card
 import com.medal.bronze.jsnader.arkhamweakness.CardType
-import java.util.*
+import kotlin.collections.ArrayList
 
+/**
+ * This is a class that is meant to represent physical locations in the game that are represented
+ * as cards.  They have names, [LocationTrait]s and [ConnectionType]s associated with them.  The
+ * name is not always unique since there could be multiple copies of each.  This should be kept in
+ * mind when using data associated with locations for filtering purposes.
+ *
+ * @author Jeremiah Snader
+ */
 class Location(pName: String?,
                pDrawableResource: Int,
-               pLocationTraits: ArrayList<LocationTrait?>?) : Card() {
+               pLocationTraits: ArrayList<LocationTrait?>?,
+               pLocationValue: ConnectionType,
+               pLocationConnections: ArrayList<ConnectionType>) : Card() {
 
     var mName: String?
     var mLocationTraits: ArrayList<LocationTrait?>?
+    val mLocType: ConnectionType
+    var mConnections: ArrayList<ConnectionType>
 
     init {
         mCardType = CardType.LOCATION
         mName = pName
         mDrawableResource = pDrawableResource
         mLocationTraits = pLocationTraits
+        mLocType = pLocationValue
+        mConnections = pLocationConnections
+    }
+
+    public fun getLocationSymbol() : ConnectionType {
+        return mLocType;
+    }
+
+    public fun getConnections() : ArrayList<ConnectionType> {
+        return mConnections
     }
 }
--- WeaknessGenerator/src/main/java/com/medal/bronze/jsnader/arkhamweakness/locations/LocationBuilder.kt
+++ WeaknessGenerator/src/main/java/com/medal/bronze/jsnader/arkhamweakness/locations/LocationBuilder.kt
@@ -1,13 +1,31 @@
+/**
+ * Copyright 2018-2021 Jeremiah Snader
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.medal.bronze.jsnader.arkhamweakness.locations
 
 import com.medal.bronze.jsnader.arkhamweakness.R
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * A class that is meant to build a list of locations that would define what should be included
  * within the ScenarioAdapter class.
  *
- * Created by Jeremiah on 3/24/2018.
+ * @author Jeremiah Snader
  */
 class LocationBuilder private constructor() {
 
@@ -15,36 +33,168 @@ class LocationBuilder private constructor() {
         val locs = ArrayList<Location?>()
         val locationTraits = ArrayList(Arrays.asList(LocationTrait.DUNWICH))
         locs.add(
-                Location("Blasted Heath", R.drawable.unseen_blasted_heath, locationTraits))
+                Location("Blasted Heath",
+                        R.drawable.unseen_blasted_heath,
+                        locationTraits,
+                        ConnectionType.SQUARE,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.CIRCLE,
+                                ConnectionType.HOURGLASS))))
         locs.add(
-                Location("Cold Spring Glen", R.drawable.unseen_cold_spring_glen, locationTraits))
+                Location(
+                        "Cold Spring Glen",
+                        R.drawable.unseen_cold_spring_glen,
+                        locationTraits,
+                        ConnectionType.TRIANGLE,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.CIRCLE,
+                                ConnectionType.DIAMOND,
+                                ConnectionType.PLUS_SIGN))))
         locs.add(
-                Location("Devil's Hopyard", R.drawable.unseen_devils_hop_yard, locationTraits))
+                Location("Devil's Hopyard",
+                        R.drawable.unseen_devils_hop_yard,
+                        locationTraits,
+                        ConnectionType.HOURGLASS,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.SQUARE,
+                                ConnectionType.PLUS_SIGN))))
         locs.add(
-                Location("Dunwich Village", R.drawable.unseen_dunwich_village, locationTraits))
+                Location("Dunwich Village",
+                        R.drawable.unseen_dunwich_village,
+                        locationTraits,
+                        ConnectionType.CIRCLE,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.TRIANGLE,
+                                ConnectionType.SQUARE,
+                                ConnectionType.DIAMOND))))
         locs.add(
-                Location("Ten Acre Meadow", R.drawable.unseen_ten_acre_meadow, locationTraits))
+                Location("Ten Acre Meadow",
+                        R.drawable.unseen_ten_acre_meadow,
+                        locationTraits,
+                        ConnectionType.TRIANGLE,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.CIRCLE,
+                                ConnectionType.TRIANGLE,
+                                ConnectionType.PLUS_SIGN))))
         locs.add(
-                Location("Whateley Ruins", R.drawable.unseen_whateley_ruins, locationTraits))
+                Location("Whateley Ruins",
+                        R.drawable.unseen_whateley_ruins,
+                        locationTraits,
+                        ConnectionType.PLUS_SIGN,
+                        ArrayList(Arrays.asList(
+                                ConnectionType.TRIANGLE,
+                                ConnectionType.DIAMOND,
+                                ConnectionType.HOURGLASS))))
         return locs
     }
 
     fun getForgottenAgeLocations(): ArrayList<Location?>? {
         val locs = ArrayList<Location?>()
         val locationTraits = ArrayList(Arrays.asList(LocationTrait.FORGOTTEN_AGE))
-        locs.add(Location("Steps of Yoth", R.drawable.location_steps_of_yoth, locationTraits))
         locs.add(Location(
-                "City of the Serpents", R.drawable.location_city_of_the_serpents, locationTraits))
-        locs.add(Location("Hall of Heresy", R.drawable.location_hall_of_heresy, locationTraits))
+                "Steps of Yoth",
+                R.drawable.location_steps_of_yoth,
+                locationTraits,
+                ConnectionType.EQUAL_SIGN,
+                ArrayList(Arrays.asList(
+                        ConnectionType.HOURGLASS,
+                        ConnectionType.SQUARE,
+                        ConnectionType.TRIANGLE,
+                        ConnectionType.DIAMOND,
+                        ConnectionType.HEART))))
         locs.add(Location(
-                "Crumbling Precipice", R.drawable.location_crumbling_precipice, locationTraits))
-        locs.add(Location("Caverns of Yoth", R.drawable.location_caverns_of_yoth, locationTraits))
-        locs.add(Location("Forked Path", R.drawable.location_forked_path, locationTraits))
+                "City of the Serpents",
+                R.drawable.location_city_of_the_serpents,
+                locationTraits,
+                ConnectionType.DIAMOND,
+                ArrayList(Arrays.asList(
+                        ConnectionType.EQUAL_SIGN,
+                        ConnectionType.FLAME,
+                        ConnectionType.TRIANGLE,
+                        ConnectionType.TETRIS_T,
+                        ConnectionType.SQUARE))))
+        locs.add(Location("Hall of Heresy",
+                R.drawable.location_hall_of_heresy,
+                locationTraits,
+                ConnectionType.TRIANGLE,
+                ArrayList(Arrays.asList(
+                        ConnectionType.EQUAL_SIGN,
+                        ConnectionType.DIAMOND,
+                        ConnectionType.CIRCLE,
+                        ConnectionType.SQUARE,
+                        ConnectionType.TETRIS_T))))
         locs.add(Location(
-                "Bridge over N'kai", R.drawable.location_bridge_over_nkai, locationTraits))
-        locs.add(Location("Broken Passage", R.drawable.location_broken_passage, locationTraits))
-        locs.add(Location("Abandoned Site", R.drawable.location_abandoned_site, locationTraits))
-        locs.add(Location("Bright Canyon", R.drawable.location_bright_canyon, locationTraits))
+                "Crumbling Precipice",
+                R.drawable.location_crumbling_precipice,
+                locationTraits,
+                ConnectionType.HOURGLASS,
+                ArrayList(Arrays.asList(
+                        ConnectionType.EQUAL_SIGN,
+                        ConnectionType.TILDA,
+                        ConnectionType.HEART,
+                        ConnectionType.TETRIS_T,
+                        ConnectionType.FLAME))))
+        locs.add(Location("Caverns of Yoth",
+                R.drawable.location_caverns_of_yoth,
+                locationTraits,
+                ConnectionType.FLAME,
+                ArrayList(Arrays.asList(
+                        ConnectionType.CIRCLE,
+                        ConnectionType.HOURGLASS,
+                        ConnectionType.HEART,
+                        ConnectionType.DIAMOND,
+                        ConnectionType.TILDA))))
+        locs.add(Location("Forked Path",
+                R.drawable.location_forked_path,
+                locationTraits,
+                ConnectionType.TETRIS_T,
+                ArrayList(Arrays.asList(
+                        ConnectionType.CIRCLE,
+                        ConnectionType.DIAMOND,
+                        ConnectionType.HOURGLASS,
+                        ConnectionType.SQUARE,
+                        ConnectionType.TRIANGLE))))
+        locs.add(Location(
+                "Bridge over N'kai",
+                R.drawable.location_bridge_over_nkai,
+                locationTraits,
+                ConnectionType.HEART,
+                ArrayList(Arrays.asList(
+                        ConnectionType.EQUAL_SIGN,
+                        ConnectionType.CIRCLE,
+                        ConnectionType.FLAME,
+                        ConnectionType.HOURGLASS,
+                        ConnectionType.TILDA))))
+        locs.add(Location("Broken Passage",
+                R.drawable.location_broken_passage,
+                locationTraits,
+                ConnectionType.TILDA,
+                ArrayList(Arrays.asList(
+                        ConnectionType.CIRCLE,
+                        ConnectionType.FLAME,
+                        ConnectionType.HOURGLASS,
+                        ConnectionType.SQUARE,
+                        ConnectionType.HEART))))
+        locs.add(Location("Abandoned Site",
+                R.drawable.location_abandoned_site,
+                locationTraits,
+                ConnectionType.SQUARE,
+                ArrayList(Arrays.asList(
+                        ConnectionType.EQUAL_SIGN,
+                        ConnectionType.DIAMOND,
+                        ConnectionType.TRIANGLE,
+                        ConnectionType.TETRIS_T,
+                        ConnectionType.TILDA))))
+        locs.add(Location("Bright Canyon",
+                R.drawable.location_bright_canyon,
+                locationTraits,
+                ConnectionType.CIRCLE,
+                ArrayList(Arrays.asList(
+                        ConnectionType.FLAME,
+                        ConnectionType.TILDA,
+                        ConnectionType.TETRIS_T,
+                        ConnectionType.HEART,
+                        ConnectionType.TRIANGLE))))
         return locs
     }
 
